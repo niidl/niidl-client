c265d7de806d3d919acc6584084573b360b532d8
Kai-Animator <kaireml@protonmail.com>
2023-03-20 15:38:40 +0900

    changed some thread stuff

--- app/project/[projectId]/discussion/[threadId]/components/NewMessage.tsx
+++ app/project/[projectId]/discussion/[threadId]/components/NewMessage.tsx
@@ -1,16 +1,26 @@
+'use client';
+
 export default function NewMessage() {
+  // function handleSubmit(e: any): void {
+  //   e.preventDefault();
+  //   const textValue = e.target.textarea.value;
+  // }
+
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
-      <textarea
-        id='message'
-        rows={4}
-        placeholder='Leave a comment...'
-        className="text-area"
-        style={{resize: 'vertical', margin: '20px', width: '600px'}}
-      ></textarea>
-      <button style={{width: '160px', margin: '0px 20px 20px'}}>
-        Click Me
-      </button>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <form>
+        <textarea
+          id='message'
+          rows={4}
+          name='textarea'
+          placeholder='Leave a comment...'
+          className='text-area'
+          style={{ resize: 'vertical', margin: '20px', width: '600px' }}
+        ></textarea>
+        <button style={{ width: '160px', margin: '0px 20px 20px' }}>
+          Click Me
+        </button>
+      </form>
     </div>
   );
 }
