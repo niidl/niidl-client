e61311d81560caccf74b05933835301f462f4c87
MrBCendales <bn.cendales10@gmail.com>
2023-03-21 20:16:36 +0900

    feat: Types on issues

--- app/project/[projectId]/components/Issues.tsx
+++ app/project/[projectId]/components/Issues.tsx
@@ -2,11 +2,24 @@ import Link from 'next/link';
 import styles from './Issues.module.scss';
 import moment from 'moment';
 
-export default function Issues({ projectIssues }: any) {
+interface Issue {
+  issue_id: number;
+  html_url: string;
+  title: string;
+  created_at: Date;
+  issue_author: string;
+  author_id: string;
+}
+
+export interface Props {
+  projectIssues: Issue[];
+}
+
+export default function Issues({ projectIssues }: Props) {
   return (
     <div className={styles.issuesContainer}>
       {projectIssues
-        ? projectIssues.map((issue: any) => {
+        ? projectIssues.map((issue: Issue) => {
             return (
               <Link href={`${issue.html_url}`} key={issue.issue_id}>
                 <div className={styles.issueInstance}>
