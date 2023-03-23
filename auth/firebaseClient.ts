01746079c1b86e5fd20b6706e88c3a9f26ecfe63
MrBCendales <bn.cendales10@gmail.com>
2023-03-23 15:38:39 +0900

    feat: Navbar bug fixed

--- auth/firebaseClient.ts
+++ auth/firebaseClient.ts
@@ -7,11 +7,11 @@ import {
 
 const CLIENT_CONFIG = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.SECRET_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.SECRET_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.SECRET_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.SECRET_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.SECRET_FIREBASE_APP_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length < 1 ? initializeApp(CLIENT_CONFIG) : undefined;
