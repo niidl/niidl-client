41df09909a72818366439845ed920d60b298b906
Kai-Animator <kaireml@protonmail.com>
2023-03-25 19:36:17 +0900

    thread

--- app/project/[projectId]/discussion/[threadId]/page.tsx
+++ app/project/[projectId]/discussion/[threadId]/page.tsx
@@ -1,10 +1,8 @@
-'use client';
 import moment from 'moment';
 import ThreadMessage from './components/ThreadMessage';
 import NewMessage from './components/NewMessage';
 import styles from './page.module.scss';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 
 interface Message {
   id: number;
@@ -27,76 +25,58 @@ interface ThreadInfo {
   user: {
     user_name: string;
   };
+
   thread_tag: string;
   upvotes: number;
   isPinned: boolean;
 }
+async function getMessages(
+  projectId: number,
+  threadId: number
+): Promise<Message[]> {
+  const res = await fetch(
+    `https://niidl.net/projects/${projectId}/threads/${threadId}/messages`,
+    { cache: 'no-store' }
+  );
+  return res.json();
+}
 
-export default function ThreadPage({ params }: any) {
-  const placeholderThread: ThreadInfo = {
-    id: 0,
-    content: '',
-    project_id: 0,
-    user_id: '',
-    creation_time: new Date(),
-    title: '',
-    user: {
-      user_name: '',
-    },
-    thread_tag: '',
-    upvotes: 0,
-    isPinned: false,
-  };
+async function getThreadInfo(
+  projectId: number,
+  threadId: number
+): Promise<ThreadInfo> {
+  const res = await fetch(
+    `https://niidl.net/projects/${projectId}/threads/${threadId}`,
+    { cache: 'no-store' }
+  );
+  return res.json();
+}
 
-  const [messages, setMessages] = useState<Message[] | Promise<any>>([
-    {
-      id: 0,
-      user_id: 0,
-      threads_id: 0,
-      content: '',
-      creation_time: new Date(),
-      user: {
-        user_name: '',
-      },
-    },
-  ]);
-  const [threadInformation, setThreadInformation] = useState<
-    ThreadInfo | Promise<any>
-  >(placeholderThread);
+export default async function ThreadPage({ params }: any) {
+  const messages: Message[] = await getMessages(
+    params.projectId,
+    params.threadId
+  );
 
-  async function getMessages(pId: number, tId: number): Promise<void> {
-    const res = await fetch(
-      `https://niidl.net/projects/${pId}/threads/${tId}/messages`
-    ).then((data) => data.json());
-    setMessages(res);
-  }
-
-  async function getThreadInfo(pId: number, tId: number): Promise<void> {
-    const res = await fetch(
-      `https://niidl.net/projects/${pId}/threads/${tId}`
-    ).then((data) => data.json());
-    setThreadInformation(res);
-  }
-
-  useEffect(() => {
-    setMessages(getMessages(params.projectId, params.threadId));
-    setThreadInformation(getThreadInfo(params.projectId, params.threadId));
-  }, []);
+  const threadInfo: ThreadInfo = await getThreadInfo(
+    params.projectId,
+    params.threadId
+  );
 
   return (
     <div className={styles.threadBody}>
       <div>
-        <Link href={`/project/${params.projectId}`}>
+        <Link href={`/project/${threadInfo.project_id}`}>
           <h4>Back to Project.</h4>
         </Link>
       </div>
-      <h1>{threadInformation.title}</h1>
+      <h1>{threadInfo.title}</h1>
       <h3>Discussion</h3>
       <div className={styles.userInfoContainer}>
-        <h3>{threadInformation.user.user_name}</h3>
-        <p>{moment(threadInformation.creation_time).fromNow()}</p>
+        <h3>{threadInfo.user.user_name}</h3>
+        <p>{moment(threadInfo.creation_time).fromNow()}</p>
       </div>
-      <p>{threadInformation.content}</p>
+      <p>{threadInfo.content}</p>
       <hr />
       {Array.isArray(messages) &&
         messages.map((message, index) => {
@@ -111,8 +91,8 @@ export default function ThreadPage({ params }: any) {
           );
         })}
       <NewMessage
-        thread_id={threadInformation.id}
-        project_id={threadInformation.project_id}
+        thread_id={threadInfo.id}
+        project_id={threadInfo.project_id}
       />
     </div>
   );
