b75bc11ab195e0d0c07f63c636d12d445064f428
Kai-Animator <kaireml@protonmail.com>
2023-03-30 14:22:21 +0900

    vercel test

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -92,8 +92,9 @@ async function getOwner(projectId: number, username: string): Promise<boolean> {
   return res.json();
 }
 
+const username: any = cookies().get('userName');
+
 export default async function ProjectPage({ params }: any) {
-  const username: any = cookies().get('userName');
   const project: SingleProj = await getProjectInfo(params.projectId);
   const tagOnly: Array<string> = project.tags.map((tag) => {
     return tag.tag_name;
