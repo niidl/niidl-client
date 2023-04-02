a6baae8bb962504c2e69c794d41dc628f2dcb7d7
MrBCendales <bn.cendales10@gmail.com>
2023-04-02 20:34:38 +0900

    feat: Loading image changed

--- app/project/[projectId]/loading.tsx
+++ app/project/[projectId]/loading.tsx
@@ -3,15 +3,6 @@ import styles from '../../loading.module.scss';
 export default function Loading() {
   let ranNum = Math.floor(Math.random() * 100);
   let loadedImage = '';
-  if (ranNum > 50) {
-    loadedImage = 'https://i.ibb.co/cQmvv4D/Bulbasaur.png';
-  } else {
-    loadedImage = 'https://i.ibb.co/FmQLV7N/Squirtle.png';
-  }
 
-  return (
-    <div className={styles.loadingImage}>
-      <img src={loadedImage} width="200px" />
-    </div>
-  );
+  return <div className={styles.loadingImage}>niidl</div>;
 }
