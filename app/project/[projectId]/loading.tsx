6e300a98cdc32b34c20e9208bbf017039dd6d251
MrBCendales <bn.cendales10@gmail.com>
2023-04-02 20:36:52 +0900

    fix: Changed Loading Image

--- app/project/[projectId]/loading.tsx
+++ app/project/[projectId]/loading.tsx
@@ -1,8 +1,7 @@
 import styles from '../../loading.module.scss';
 
 export default function Loading() {
-  let ranNum = Math.floor(Math.random() * 100);
-  let loadedImage = '';
-
+  /*let ranNum = Math.floor(Math.random() * 100);
+  let loadedImage = '';*/
   return <div className={styles.loadingImage}>niidl</div>;
 }
